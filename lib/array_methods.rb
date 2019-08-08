eba0a266585b51c2a928a1313cf0c76c730cbc70
Christina Gonzalez <cmgamway@gmail.com>
2019-08-08 06:44:25 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,24 +1,20 @@
 def using_include(array, element)
-  when "takes in arguments of an array and an element and returns true if the array includes that element, false if it does not." do
   array.include?("am")
   array.include?("great")
 
 end
 
 def using_sort(array)
-  when "takes in an argument of an array and returns the sorted array." do
     array.sort
 
 end
 
 def using_reverse(array)
-  when "takes in argument of an array and returns that same array with the contents reversed." do
     array.reverse
 
 end
 
 def using_first(array)
-  when "takes in argument of an array and and returns the first element in the array" do
     array.first
 
 end
